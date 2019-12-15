2d38cc33a71a587a8326e2af61ec4b2b30ef4071
yangbo <1051435548@qq.com>
2019-12-15 18:31:55 +0800

    Initial commit

---
+++ src/main/java/com/young/asow/AsowApplication.java
@@ -0,0 +1,13 @@
+package com.young.asow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/young/asow/AsowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.young.asow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
